acd80a4eba975ca736f46f8a1b5bb82863de2bd5
Mayank <smayank748@gmail.com>
2025-07-13 00:24:40 +0530

    refactored and corrected the code

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import notfound from "@/assets/pageNotFound.gif";
+import './NotFund.css';
 
 const NotFound = () => {
   const location = useLocation();
@@ -12,10 +14,14 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="backgroundColor min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
+        <p className="text-xl text-gray-600 mb-4"><img 
+                  src={notfound} 
+                  alt="Not found"
+                  className="w-full h-48 object-cover transition-transform duration-500 group-hover:scale-110"
+                /></p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
           Return to Home
         </a>
--- vite.config.ts
+++ vite.config.ts
@@ -63,6 +63,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: '/',
+  base: '/sasrmWeb/',
 });
 
